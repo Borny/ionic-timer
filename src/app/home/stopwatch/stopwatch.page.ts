1a160ce4af42491302ce3d1774fa61c2718415d9
Borny <borntowebb@gmail.com>
2021-07-15 09:26:18 +0200

    Added the circle progress to the stopwatch page

--- src/app/home/stopwatch/stopwatch.page.ts
+++ src/app/home/stopwatch/stopwatch.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnDestroy } from '@angular/core';
 
 import { interval, Subscription } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-stopwatch',
@@ -8,13 +9,28 @@ import { interval, Subscription } from 'rxjs';
   styleUrls: ['./stopwatch.page.scss'],
 })
 export class StopwatchPage implements OnDestroy {
-
   public clock = 0;
   public pause = true;
+  public start = true;
+  public radius = 100;
+  public subtitle: string;
+  public percent = 100;
+
+  public readonly PRIMARY_COLOR = '#428cff';
+  public readonly SECONDARY_COLOR = '#50c8ff';
+  public readonly DANGER_COLOR = '#ff4961';
+
+  private readonly START = 'Start';
+  private readonly PAUSED = 'Paused';
+  private readonly ON = 'On';
 
   private _intervalObs$: Subscription;
 
-  constructor() { }
+  constructor() {}
+
+  ngOnInit(): void {
+    this.getSubtitle();
+  }
 
   ionViewDidLeave() {
     this.onReset();
@@ -24,22 +40,41 @@ export class StopwatchPage implements OnDestroy {
     this.onReset();
   }
 
-  public onToggle(pause: boolean): void {
-    this.pause = pause;
-    if (pause) {
+  public onToggle(pause?: boolean): void {
+    this.pause = !this.pause;
+    this.start = false;
+    this.getSubtitle();
+    if (this.pause && this._intervalObs$) {
       this._intervalObs$.unsubscribe();
       return;
     }
-    this._intervalObs$ = interval(1000).subscribe(() => {
-      this.clock++;
-    });
+    this._intervalObs$ = interval(1000)
+      .pipe(
+        tap(() => {
+          this.clock++;
+          console.log();
+        })
+      )
+      .subscribe();
   }
 
   public onReset(): void {
     this.pause = true;
+    this.start = true;
+    this.getSubtitle();
     if (this._intervalObs$) {
       this._intervalObs$.unsubscribe();
     }
     this.clock = 0;
+  }
+
+  public getSubtitle(): string {
+    if (this.start) {
+      return (this.subtitle = this.START);
+    } else if (this.pause && !this.start) {
+      return (this.subtitle = this.PAUSED);
+    } else {
+      return (this.subtitle = this.ON);
+    }
   }
 }
